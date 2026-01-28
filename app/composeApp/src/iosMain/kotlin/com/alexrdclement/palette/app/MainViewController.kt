516771bbfb5c607907890420bd9936a66de53bee
Alex Clement <alexrdclement@gmail.com>
2026-01-28 08:35:22 -0500

    Remove EndEdgePanGestureBehavior configuration

--- app/composeApp/src/iosMain/kotlin/com/alexrdclement/palette/app/MainViewController.kt
+++ app/composeApp/src/iosMain/kotlin/com/alexrdclement/palette/app/MainViewController.kt
@@ -1,15 +1,8 @@
 package com.alexrdclement.palette.app
 
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.uikit.EndEdgePanGestureBehavior
 import androidx.compose.ui.window.ComposeUIViewController
 import platform.UIKit.UIViewController
 
-@OptIn(ExperimentalComposeUiApi::class)
-fun MainViewController(): UIViewController = ComposeUIViewController(
-    configure = {
-        endEdgePanGestureBehavior = EndEdgePanGestureBehavior.Back
-    }
-) {
+fun MainViewController(): UIViewController = ComposeUIViewController {
     App()
 }
